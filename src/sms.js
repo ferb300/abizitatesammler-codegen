f16abb794a1cd5f224dada8a6d8499c5892a9b61
Moritz Tietze <moritz.tietze@icloud.com>
2022-02-21 19:44:48 +0100

    switched from messente to vonage

--- src/sms.js
+++ src/sms.js
@@ -1,25 +1,22 @@
-import MessenteApi from "messente_api"
+import Vonage from "@vonage/server-sdk"
 
-const defaultClient = MessenteApi.ApiClient.instance;
-const basicAuth = defaultClient.authentications["basicAuth"];
-basicAuth.username = process.env.MESSENTE_USERNAME;
-basicAuth.password = process.env.MESSENTE_PASSWORD;
-
-const api = new MessenteApi.OmnimessageApi();
+const vonage = new Vonage({
+    apiKey: process.env.API_KEY,
+    apiSecret: process.env.API_SECRET
+})
 
 export default function sendSMS(number, text, sandbox) {
-    //console.log(`${sandbox ? "[SANDBOX] ": ""} Sent SMS to ${number}: \"${text}\"`)
+    console.log(`${sandbox ? "[SANDBOX] " : ""} Sent SMS to ${number}: \"${text}\"`)
     if (sandbox) { return }
-    let sms = MessenteApi.SMS.constructFromObject({
-        text: text
-    });
-    let omnimessage = MessenteApi.Omnimessage.constructFromObject({
-        messages: [sms],
-        to: number,
-    });
-    api.sendOmnimessage(omnimessage, (error, data) => {
-        if (error) {
-            console.error(error);
+    vonage.message.sendSms("Abibot", number, text, (err, responseData) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if(responseData.messages[0]['status'] === "0") {
+                console.log("Message sent successfully.");
+            } else {
+                console.log(`Message failed with error: ${responseData.messages[0]['error-text']}`);
+            }
         }
-    });
+    })
 }
